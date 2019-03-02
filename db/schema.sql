4687c26c154858ecc696c54b100aa27f58818e90
christopherdonner <christopher.donner@gmail.com>
2019-03-02 18:48:27 -0500

    some funcitonality

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+drop database burgers_db;
+create database burgers_db;
+
+use burgers_db;
+create table burgers
+(
+    id int not null auto_increment,
+    burgerName varchar (64),
+    devoured boolean,
+    primary key (id)
+);
