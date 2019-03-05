3e18b0e5f81e06ecf2cf95fe8661231656a1743a
Christopher Donner <christopher.donner@gmail.com>
2019-03-05 09:52:49 -0500

    burger add working, devour button firing

--- config/orm.js
+++ config/orm.js
@@ -1,3 +1,16 @@
-* `selectAll()`
-* `insertOne()`
-* `updateOne()`
+// * `selectAll()`
+// * `insertOne()`
+// * `updateOne()`
+var connection = require("./connection.js");
+
+var orm = {
+    selectAll: function(){
+    var queryString=""
+    },
+    insertOne: function(){
+    var queryString=""
+    },
+    updateOne: function(){
+    var queryString=""
+    }
+}
---
+++ models/burger.js
@@ -0,0 +1,4 @@
+module.exports=function(sequelize, DataTypes) {
+var burgers = sequelize.define("Burgers". {
+    IF: DataTypes.INTEGER
+})
